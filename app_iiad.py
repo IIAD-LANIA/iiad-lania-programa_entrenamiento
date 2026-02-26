c9654901b765aad08419e6a000923f3b26fab2dd
Iván Mauricio Huérfano Barco <imhuerfanob@libertadores.edu.co>
2026-02-26 14:19:37 -0500

    Update app_iiad.py

--- app_iiad.py
+++ app_iiad.py
@@ -509,8 +509,8 @@ def inject_css():
 # PÁGINA 1 — DASHBOARD
 # ─────────────────────────────────────────────────────────────────────────────
 def pagina_dashboard():
-    st.title("Dashboard — Sistema de Formación IIAD")
-    st.markdown('## Basado en las normas ISO 17034 / ISO 17043 / ISO 13528')
+    st.markdown("#Dashboard — Sistema de seguimiento a la formación del personal del área IIAD")
+    st.markdown('### Basado en las normas ISO 17034 / ISO 17043 / ISO 13528')
     st.caption(f"📅 Actualizado: {datetime.now().strftime('%d/%m/%Y %H:%M')}")
 
     personal = get_personal()
@@ -948,6 +948,7 @@ def main():
             </div>
         </div>
         """, unsafe_allow_html=True)
+        st.markdown('## Área de Investigación e Innovación Analítica y Diagnóstica')
 
         # ── Menú de navegación ────────────────────────────────────────────────
         pagina = st.radio(
